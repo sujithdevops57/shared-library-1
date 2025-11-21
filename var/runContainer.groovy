ae1f90bff3df37cb676843968de23a571b3652ea
sujithdevops57 <sujithmanelli5657@gmail.com>
2025-11-21 14:31:45 +0530

    Create runContainer.groovy

---
+++ var/runContainer.groovy
@@ -0,0 +1,11 @@
+def call(String imageName, String containerName, int hostPort, int containerPort) {
+    stage('Docker Run') {
+        echo "Running container '${containerName}' from image '${imageName}' ..."
+        sh """
+           # Run new container
+            echo "Starting new container..."
+            docker run -d --name ${containerName} -p ${hostPort}:${containerPort} ${imageName}
+        """
+        echo "Container '${containerName}' is running on port ${hostPort}"
+    }
+}
